4689998e6b7dd8e3103a7f26d4dd3a195ead68d8
Pyowoojin <20185291@kookmin.ac.kr>
2022-05-27 14:10:34 +0900

    c

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const app = express();
 const port = process.env.PORT || 8082;
 
 const getRouter = require('./routes/get_rfid');
-// const postRouter = require('./routes/post_rfid');
 const gettest = require('./routes/test_rfid');
 
 app.use('/id', getRouter);
-// app.use('/rfid', postRouter);
 app.use('/test/newtest', gettest);
 
 app.get('/', (req, res)=> {
     console.log('who get in root');
-    res.send("hello root");
+    res.send('/root');
 });
 
 app.use((req, res, next) => {
