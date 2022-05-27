7ee4677e77ebf5d384c5e7b2f902cb14587723a3
Pyowoojin <20185291@kookmin.ac.kr>
2022-05-27 14:05:08 +0900

    c

--- routes/get_rfid.js
+++ routes/get_rfid.js
@@ -1,21 +1,43 @@
 const express = require("express");
-const con = require('./post_rfid');
+// const con = require('./post_rfid');
 const db = require('../schemas/rfid');
 const re = require('../schemas/dijk');
+const connect = require('../schemas/mongod'); // 수정
 const fs = require('fs');
 const router = express.Router();
 
 
 var cnt;
 var info;
+re.count().then(function(c, err){
+    cnt = c;
+ });
+
+ createtxt = function(){
+    fs.writeFile('./test.txt', "", (err) => {
+        if(err)
+            return console.log(err);
+        console.log('New file has been created');
+    });
+ };
 
 router.get("/?", (req, res) => {
     let tempurl = req.url;
     let tag1 = req.query.store1;
     let tag2 = req.query.store2;
     // console.log(tempurl); console.log(tag1, tag2);
-    re.count().then(function(c, err){
-       cnt = c;
+
+    fs.access('./test.txt', fs.constants.F_OK, (err)=>{
+        if (err){
+            fs.writeFile('./test.txt', "", (err) => {
+                if(err)
+                    return console.log(err);
+                console.log('New file has been created');
+            });
+        }
+        else{
+            fs.unlink('./test.txt', (err) => createtxt() ? console.log(err) : console.log('삭제완료'));
+        }
     });
 
     // 모든 노드의 nodeid, linkedNode는 출력 _id값은 출력 X
@@ -24,7 +46,9 @@ router.get("/?", (req, res) => {
             throw err;
         // 전체 Collection 정보를 담은 Object
         info = okk;
+        res.send(okk);
         // Dijkstra 알고리즘 수행을 위한 txt파일 생성
+
         for(let j = 0; j<cnt; j++){
             arr = String(info[j].nodeid);
             for(let i = 0; i < (Object.keys(info[j].linkedNode)).length; i++){
