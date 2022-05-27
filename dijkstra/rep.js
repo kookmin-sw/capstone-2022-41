fadf0198b4b2cb5e60bca5c1d97d744d716c5f81
Pyowoojin <20185291@kookmin.ac.kr>
2022-05-27 14:57:02 +0900

    .

--- dijkstra/rep.js
+++ dijkstra/rep.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 let txtarr = [];
+var line;
 
 // 노드 정보 저장된 파일을 가져와서 배열로 만들어 줄 것임
 // 목적은 중복된 데이터를 제거하고 간선의 개수를 구하기 위해
@@ -12,17 +13,21 @@ fs.readFile('../test.txt', 'utf-8', (err, data) => {
         txtarr.push(arr[i].split(','));
     };
     // 맨 마지막 null값은 없애줄거임
-    //txtarr.splice(txtarr.length-1, 1);
     txtarr.pop();
     console.log(txtarr);
 
     // 중복값 제거 코드
-    /*let keyP = 0;
     for (let i = 0; i < txtarr.length; i++){
-        if(txtarr[i][0] == txtarr[])
-    }*/
-    delete txtarr[0];
-    txtarr[1] = null;
+        for(let j = i+1; j < txtarr.length; j++){
+            if((txtarr[i] != undefined) && (String(txtarr[i][0]) == String(txtarr[j][1]))){
+                txtarr.splice(j,1);
+            }
+        }
+    }
     console.log(txtarr);
+    line = txtarr.length;
+    console.log(line);
 
-})
+}).then(function(result){
+    console.log(line);
+});
--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
     res.status(404).send('Nottttt Found');
 });
 
-app.listen(port, ()=>{
-    console.log('Server is running on' + port);
+pp.listen(port, ()=>{
+    console.log('Server is running on ' + port);
 });
