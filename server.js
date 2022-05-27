93065986ebb1c4a17b3bd81084ddf63ff88e4d31
Pyowoojin <66067291+Pyowoojin@users.noreply.github.com>
2022-05-27 12:32:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 80;
 
 const getRouter = require('./routes/get_rfid');
 // const postRouter = require('./routes/post_rfid');
@@ -12,7 +12,6 @@ app.use('/test/newtest', gettest);
 
 app.get('/', (req, res)=> {
     console.log('who get in root');
-    res.send("hello root");
 });
 
 app.use((req, res, next) => {
@@ -20,5 +19,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, ()=>{
-    console.log('Example app listening on port ' + port);
+    console.log('Server is running on' + port);
 });
